eec1d16bada733d008c39807cf51b8f223fa964c
m s <syed_m8@yahoo.com>
2025-06-08 21:21:07 -0400

    first commit, loading page

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen';
+import "./index.css";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
+    <>
+    {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>}
+    </>
+  );
+}
+
+export default App

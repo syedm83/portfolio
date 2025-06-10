326bb765a432a0ea446bac78b8b06419bc5731b8
m s <syed_m8@yahoo.com>
2025-06-10 14:31:17 -0400

    fixed bug

--- portfolio/src/components/sections/Contact.jsx
+++ portfolio/src/components/sections/Contact.jsx
@@ -19,7 +19,7 @@ export const Contact = () =>{
         }).catch(() => alert("Something went wrong. Please try again."));
     };
 
-    return <section id="contact" className="min-h-screen flex items-center justify-center py-20">
+    return <section id="contact" className="min-h-screen flex items-center justify-center mx-10 px-20 py-20">
         <RevealOnScroll>
             <div className="px-4 w-150">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-br from-teal-300 to-indigo-400 bg-clip-text text-transparent text-center"> 

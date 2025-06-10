e8ca4e5df1bca587f2b06c102945b783b59c3b79
m s <syed_m8@yahoo.com>
2025-06-10 14:55:19 -0400

    fixed contact

--- portfolio/src/components/sections/Contact.jsx
+++ portfolio/src/components/sections/Contact.jsx
@@ -21,7 +21,7 @@ export const Contact = () =>{
 
     return <section id="contact" className="min-h-screen flex items-center justify-center py-20">
         <RevealOnScroll>
-            <div className="px-4 w-150">
+            <div className="w-full max-w-xl px-4 mx-auto">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-br from-teal-300 to-indigo-400 bg-clip-text text-transparent text-center"> 
                     Get in touch with me!</h2>
                 <form className="space-y-6" onSubmit={handleSubmit}>

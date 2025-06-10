39c1fd4d95267b157a7e036cefde32b62f49ac4f
m s <syed_m8@yahoo.com>
2025-06-10 00:04:14 -0400

    finished contact page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import { MobileMenu } from './components/MobileMenu';
 import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
 import { Projects } from './components/sections/Projects';
+import { Contact } from './components/sections/Contact';
 import "./index.css";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
 
 
       </div>
